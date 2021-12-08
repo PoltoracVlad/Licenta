89c1f379520795bee92b985a2863ab47456acbfa
vladp <vlad.poltorac@gmail.com>
2021-12-08 17:12:29 +0200

    Licenta

--- app/src/main/java/com/example/licenta/NormalUser.java
+++ app/src/main/java/com/example/licenta/NormalUser.java
@@ -1,13 +1,16 @@
 package com.example.licenta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
 
-public class NormalUser extends AppCompatActivity {
+public class NormalUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -19,10 +22,40 @@ public class NormalUser extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new SendLocationFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_location);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_medical_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MedicalProfileFragment()).commit();
+                break;
+            case R.id.nav_location:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SendLocationFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
