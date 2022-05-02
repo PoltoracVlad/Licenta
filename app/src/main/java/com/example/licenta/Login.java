e5d69ea033f286edb22edf65f73d5a73818fa49d
vladp <vlad.poltorac@gmail.com>
2022-05-02 22:47:46 +0300

    Licenta

--- app/src/main/java/com/example/licenta/Login.java
+++ app/src/main/java/com/example/licenta/Login.java
@@ -1,19 +1,13 @@
 package com.example.licenta;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
@@ -36,49 +30,30 @@ public class Login extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         goToRegister = findViewById(R.id.goToRegister);
 
-        goToRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Register.class));
-            }
-        });
+        goToRegister.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Register.class)));
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkField(email);
-                checkField(password);
+        loginBtn.setOnClickListener(v -> {
+            checkField(email);
+            checkField(password);
 
-                if (valid) {
-                    firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                            checkTypeOfUser(authResult.getUser().getUid());
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(Login.this, "The Email or Password is incorrect", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
+            if (valid) {
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(authResult -> {
+                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    checkTypeOfUser(authResult.getUser().getUid());
+                }).addOnFailureListener(e -> Toast.makeText(Login.this, "The Email or Password is incorrect", Toast.LENGTH_SHORT).show());
             }
         });
     }
 
     private void checkTypeOfUser(String uid) {
         DocumentReference df = firebaseFirestore.collection("Users").document(uid);
-        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                String document = documentSnapshot.getString("TypeOfUser");
-                if (document.equals("NormalUser")) {
-                    startActivity(new Intent(getApplicationContext(), NormalUser.class));
-                }
-                else if (document.equals("MedicalUser")) {
-                    startActivity(new Intent(getApplicationContext(), MedicalUser.class));
-                }
+        df.get().addOnSuccessListener(documentSnapshot -> {
+            String document = documentSnapshot.getString("TypeOfUser");
+            if (document.equals("NormalUser")) {
+                startActivity(new Intent(getApplicationContext(), NormalUser.class));
+            }
+            else if (document.equals("MedicalUser")) {
+                startActivity(new Intent(getApplicationContext(), MedicalUser.class));
             }
         });
     }
--- app/src/main/java/com/example/licenta/SendLocationFragment.java
+++ app/src/main/java/com/example/licenta/SendLocationFragment.java
@@ -1,17 +1,114 @@
 package com.example.licenta;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class SendLocationFragment extends Fragment {
+
+    Button btnLocation;
+    TextView txt1, txt2, txt3, txt4, txt5;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_send_location, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        btnLocation = (Button) getView().findViewById(R.id.btn_location);
+        txt1 = (TextView) getView().findViewById(R.id.txt_location1);
+        txt2 = (TextView) getView().findViewById(R.id.txt_location2);
+        txt3 = (TextView) getView().findViewById(R.id.txt_location3);
+        txt4 = (TextView) getView().findViewById(R.id.txt_location4);
+        txt5 = (TextView) getView().findViewById(R.id.txt_location5);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        btnLocation.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 100);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+            Location location = task.getResult();
+            if (location != null) {
+                try {
+                    Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                    List<Address> addresses = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1
+                    );
+                    txt1.setText(Html.fromHtml(
+                            "<font color='#6200EE'><b>Latitude: </b><br></font>"
+                                    + addresses.get(0).getLatitude()
+                    ));
+                    txt2.setText(Html.fromHtml(
+                            "<font color='#6200EE'><b>Longitude: </b><br></font>"
+                                    + addresses.get(0).getLongitude()
+                    ));
+                    txt3.setText(Html.fromHtml(
+                            "<font color='#6200EE'><b>Country: </b><br></font>"
+                                    + addresses.get(0).getCountryName()
+                    ));
+                    txt4.setText(Html.fromHtml(
+                            "<font color='#6200EE'><b>Locality: </b><br></font>"
+                                    + addresses.get(0).getLocality()
+                    ));
+                    txt5.setText(Html.fromHtml(
+                            "<font color='#6200EE'><b>Address: </b><br></font>"
+                                    + addresses.get(0).getAddressLine(0)
+                    ));
+
+                    String phoneNumber = "+40744588005";
+                    String SMS = "I need your help, I just had a car accident! Here is my location:\n" + "\nLatitude: " + addresses.get(0).getLatitude() + "\nLongitude: " + addresses.get(0).getLongitude() + "\nCountry: " + addresses.get(0).getCountryName() + "\nLocality: " + addresses.get(0).getLocality();
+
+                    try {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNumber, null, SMS, null, null);
+                        Toast.makeText(getActivity(), "Message sent", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(getActivity(), "Failed to send message", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
